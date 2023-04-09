1a6998ad1b367da643dcbfe37bf75ab09b47f706
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-04-09 21:21:30 +0100

    footer added

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        inspired by{" "}
+        <a href="https://dev.to/ganeshmani/building-a-piano-with-react-hooks-3mih">
+          GaneshMani
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
